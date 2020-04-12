2da8865116505f2eefee8eca48ec72f15776fcef
Tijn Buijs <coloclue@cybertinus.nl>
2020-04-12 12:04:18 +0200

    Add support for nginx in the discovery script, and prevented some code repetition

--- zabbix_ssl_cert_discovery.sh
+++ zabbix_ssl_cert_discovery.sh
@@ -5,8 +5,16 @@ tempfile=$(mktemp)
 
 trap "rm -rf "${tempfile}";" EXIT SIGINT SIGTERM SIGKILL
 
+function to_json_objects() {
+	for certpath in ${1}; do
+		echo -n "${certpath}" | sed 's/^/{"{#CERTPATH}": "/;s/$/"},/'
+	done
+
+}
+
 # Start the JSON output
 {
+	# JSON start
 	echo -n '['
 
 	##########
@@ -23,12 +31,18 @@ trap "rm -rf "${tempfile}";" EXIT SIGINT SIGTERM SIGKILL
 
 	# Find the certificates for Apache, if it is installed
 	if [ -n "${apacheconfdir}" ] ; then
-		certpaths="$(grep -r SSLCertificateFile /etc/apache2 | grep -v 'default-ssl.conf' | awk '!/#/ {print $3}' | sort | uniq)"
-		for certpath in ${certpaths}; do
-			echo -n "${certpath}" | sed 's/^/{"{#CERTPATH}": "/;s/$/"},/'
-		done
+		to_json_objects "$(grep -r SSLCertificateFile /etc/apache2 | grep -v 'default-ssl.conf' | awk '!/#/ {print $3}' | sort | uniq)"
 	fi
 
+	#########
+	# NGINX #
+	#########
+	# Find the certificates for Nginx, if it is installed
+	if [ -d "/etc/nginx" ] ; then
+		to_json_objects "$(grep -r ssl_certificate /etc/nginx/ | awk '!/ssl_certificate_key/ {print $3}' | sed 's/;$//' | sort | uniq)"
+	fi
+
+	# JSON end
 	echo -n ']'
 } > "${tempfile}"
 
